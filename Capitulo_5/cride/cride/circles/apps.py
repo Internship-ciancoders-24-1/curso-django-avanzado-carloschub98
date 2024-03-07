738c688d3bb4d715145b40079b703e5d5671dff5
Carlos_Chub <carloschub9805@gmail.com>
2024-03-07 13:14:40 -0600

    Dia-4, Capitulo 5 inicio

---
+++ Capitulo_5/cride/cride/circles/apps.py
@@ -0,0 +1,10 @@
+"""App de circulos"""
+
+# Django
+from django.apps import AppConfig
+
+class CirclesAppConfig(AppConfig):
+    """Configuracion de app Circulos"""
+    
+    name = 'cride.circles' #modulo en el que esta mas la app
+    verbose_name = 'Circles'
